b3931ff55fca781c1ab4f832b5a9cc939072f2ce
Rakesh Niraula <rakeshhniraulaa@gmail.com>
2020-07-03 16:42:53 +0545

    add app icon

---
+++ app/src/main/java/com/example/fooderyadmin/ui/main/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.fooderyadmin.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fooderyadmin.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        setTheme(R.style.AppTheme)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
